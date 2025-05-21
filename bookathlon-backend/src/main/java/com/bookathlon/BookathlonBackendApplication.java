cc2ee9e7a9b838afb2c41054a74e22a3f9eccba3
RobertSSmau <robert.smau@edu-its.it>
2025-05-21 20:28:30 +0200

    chore: backend Spring Boot inizializzata
    E' vuota e prototipale, da ora in poi ogni codice solo in un futuro branch develop, in main solo codice FUNZIONANTE.
    
    Progetto spring inizializzato con:
    - Spring Web
    - Spring Data JPA
    - MySQL Driver
    - Spring Security
    - Thymeleaf
    - Validation (da researchare)

---
+++ bookathlon-backend/src/main/java/com/bookathlon/BookathlonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookathlon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookathlonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookathlonBackendApplication.class, args);
+	}
+
+}
---
+++ bookathlon-backend/src/test/java/com/bookathlon/BookathlonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookathlon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookathlonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
