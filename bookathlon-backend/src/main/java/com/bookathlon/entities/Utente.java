ac986727080aa9b3007568f9312b7e091507ab48
noemirenna <noemi.renna@edu-its.it>
2025-05-29 11:00:27 +0200

    aggiunto controller login
    creata classe per il login

--- bookathlon-backend/src/main/java/com/bookathlon/entities/Utente.java
+++ bookathlon-backend/src/main/java/com/bookathlon/entities/Utente.java
@@ -3,24 +3,25 @@ package com.bookathlon.entities;
 import com.bookathlon.enums.Ruolo;
 import jakarta.persistence.*;
 
-@Entity
-@Table(name = "utente")
+@Entity   // Indica che questa classe rappresenta una tabella nel database
+@Table(name = "utente")   // Specifica che è collegata alla tabella chiamata "utente"
 public class Utente {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id  // Questo campo è la chiave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Il valore viene generato automaticamente dal DB,autoincremento
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true)  // Lo username è un campo obbligatorio e deve essere unico nel DB
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false)  //La password è obbligatoria e deve essere cifrata prima di salvarla nel DB
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)  // Salva il ruolo come stringa ( "ADMIN" o "USER" )
+    @Column(nullable = false)  // Campo obbligatorio
     private Ruolo ruolo;
 
+// I getter e setter servono per accedere o modificare i valori degli attributi.
     public Long getId() {
         return id;
     }
--- bookathlon-backend/src/main/java/com/bookathlon/enums/Ruolo.java
+++ bookathlon-backend/src/main/java/com/bookathlon/enums/Ruolo.java
@@ -1,8 +1,9 @@
 package com.bookathlon.enums;
 
+// Classe usata per indicare il ruolo di un utente e puo avere solo valori ("ADMIN" o "USER")
 public enum Ruolo {
 	
-	USER,
-	ADMIN
+	USER, // Rappresenta un utente normale (es. lettore)
+	ADMIN // Rappresenta un amministratore del sistema
 
 }
