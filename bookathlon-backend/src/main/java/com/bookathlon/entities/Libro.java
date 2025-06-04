fddb8d3ced16a479edd5cc02c5985b88beeb3b66
glalganie <glalganie.tchissambo@edu-its.it>
2025-06-04 23:07:45 +0200

    Aggiunto javadoc e commenti

--- bookathlon-backend/src/main/java/com/bookathlon/entities/Libro.java
+++ bookathlon-backend/src/main/java/com/bookathlon/entities/Libro.java
@@ -3,24 +3,33 @@ package com.bookathlon.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-@Entity
-@Table(name="libro")
+/**
+ * Rappresenta un'entità Libro da salvare nel database
+ * Ogni libro ha un ID, un codice ISBN, un titolo, un autore e un anno di pubblicazione
+ */
+@Entity // indica che la classe è una tabella del database
+@Table(name="libro")// Mappa la classe alla tabella "libro" nel database
 
 public class Libro {
 	
 	@Id
-	private int id;
-	private String isbn;
-	private String titolo;
-	private String autore;
-	private int anno_pubblicazione;
+	private int id; // Identificativo univoco del libro (chiave primaria)
+	private String isbn; // codice ISBN del libro
+	private String titolo; // Titolo del libro
+	private String autore; // Nome dell'autore del libro
+	private int anno_pubblicazione; // Anno in cui il libro è stato pubblicato
+
+	
 	public int getId() {
 		return id;
 	}
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	
 	public String getIsbn() {
 		return isbn;
 	}
