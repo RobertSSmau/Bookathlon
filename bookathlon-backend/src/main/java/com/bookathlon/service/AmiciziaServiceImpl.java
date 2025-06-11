01db5b95d8415adae41e9b14552cf2dbaae86364
Stefano Demaria <stefano.demaria@edu-its.it>
2025-06-11 22:31:54 +0200

    Aggiornati metodi AmiciziaServiceImpl
    Aggiornati metodi per trovare amici, trovare richieste ricevute, richieste inviate e inviare richiesta amicizia

--- bookathlon-backend/src/main/java/com/bookathlon/service/AmiciziaServiceImpl.java
+++ bookathlon-backend/src/main/java/com/bookathlon/service/AmiciziaServiceImpl.java
@@ -13,28 +13,28 @@ public class AmiciziaServiceImpl implements AmiciziaService {
 	private AmiciziaRepo repo;
 	
 	@Override
-	public List<Amicizia> getAmici(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<Amicizia> getAmici(Long userId) {
+        return repo.trovaAmiciAccettati(userId);
+    }
 
-	@Override
-	public List<Amicizia> getRichiesteRicevute(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Amicizia> getRichiesteRicevute(Long userId) {
+        return repo.trovaRichiesteRicevute(userId);
+    }
 
-	@Override
-	public List<Amicizia> getRichiesteInviate(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Amicizia> getRichiesteInviate(Long userId) {
+        return repo.trovaRichiesteInviate(userId);
+    }
 
-	@Override
-	public void inviaRichiesta(Long daUtenteId, Long aUtenteId) {
-		// TODO Auto-generated method stub
-
-	}
+    @Override
+    public void inviaRichiesta(Long daUtenteId, Long aUtenteId) {
+        Amicizia a = new Amicizia();
+        a.setUtente1(daUtenteId);
+        a.setUtente2(aUtenteId);
+        a.setStato("PENDING");
+        repo.save(a);
+    }
 
 	@Override
 	public void accettaRichiesta(Long daUtenteId, Long aUtenteId) {
